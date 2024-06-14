dc7a656dc25d7ec5759e149f33dbfd1fa3feefb6
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-06-14 22:00:48 +1000

    Import json data into the model

--- films/urls.py
+++ films/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import homepage, film_detail
+from .views import add_films, homepage, film_detail
 
 app_name = "films"
 
 urlpatterns = [
     path("", homepage, name="home"),
     path("film/<int:pk>/", film_detail, name="f_detail"),
+    path("add_films", add_films, name="add_films"),
 ]
--- films/views.py
+++ films/views.py
@@ -1,6 +1,44 @@
+from datetime import timedelta
+import json
+import os
+
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Film, Rating
+
+
+module_dir = os.path.dirname(__file__)
+
+
+def add_films(request):
+    file_path = os.path.join(module_dir, "imdb_top_films.json")
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+        no_added = 0
+        for film in data:
+            dur = film["Runtime"]
+            f_dur = timedelta(minutes=int(dur[0:-4]))
+
+            Film.objects.update_or_create(
+                title=film["Series_Title"],
+                released=f"{film['Released_Year']}-01-01",
+                certificate=film["Certificate"],
+                duration=f_dur,
+                genre=film["Genre"],
+                director=film["Director"],
+                star1=film["Star1"],
+                star2=film["Star2"],
+                star3=film["Star3"],
+                star4=film["Star4"],
+                overview=film["Overview"],
+                poster=film["Poster_Link"],
+            )
+            no_added += 1
+
+        msg = f"Added {no_added} films to the database."
+        return HttpResponse(msg, content_type="text/plain")
 
 
 def homepage(request):
